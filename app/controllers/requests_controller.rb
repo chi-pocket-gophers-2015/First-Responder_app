11f59e88d3e54b0bff026dbc204b809e1b8ab797
petermanhart <petermanhart@gmail.com>
2015-05-30 11:25:07 -0500

    update routes and controller actions, fill out graffiti form

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -10,6 +10,34 @@ class RequestsController < ApplicationController
     @request = Request.new
   end
 
+  def graffiti_form 
+    @request = Request.new   
+  end
+
+  def pothole_form 
+    @request = Request.new   
+  end
+
+  def rodent_form 
+    @request = Request.new   
+  end
+
+  def street_light_form 
+    @request = Request.new   
+  end
+
+  def tree_debris_form 
+    @request = Request.new   
+  end
+
+  def tree_trim_form 
+    @request = Request.new   
+  end
+
+  def abandoned_vehicle_form 
+    @request = Request.new   
+  end
+
   def create
     binding.pry
   end
