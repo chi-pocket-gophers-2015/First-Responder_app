91a684394c498a291cce0fb47eda5c3721c5d2d2
srgupta <sendsahilemail@gmail.com>
2015-05-31 11:06:41 -0500

    pull out select options from views into model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,15 @@
 class Request < ActiveRecord::Base
 
+  POTHOLE_LOCATION_OPTIONS = ['', 'Crosswalk', 'Curb Lane', 'Intersection', 'Traffic Lane')
+
+  GRAFFITI_LOCATION_OPTIONS = ['Alley', 'Bench', 'Door', 'Dumpster', 'Express Way Job', 'Fence', 'Front', 'Garage', 'Garbage Cart', 'Hydrant', 'Mailbox',
+  'Newspaper Box', 'Phone', 'Pole', 'Rear', 'Side', 'Sign', 'Traffic Control Box']
+
+  GRAFFITI_SURFACE_OPTIONS = ['Aluminum Siding', 'Asphalt', 'Brick - Painted', 'Brick - Unpainted', 'Cement (Sidewalk, Alley, Wall, or Curb)', 'Glass', 'Limestone',
+  'Marble/Granite','Metal - Painted', 'Metal - Unpainted', 'Other/Unknown Surface', 'Stucco', 'Tree', 'Vinyl Siding', 'Wood - Painted',
+  'Wood - Unpainted']
+
+  GRAFFITI_HEIGHT_OPTIONS = ['No', 'Over 6 ft']
 
   VEHICLE_MAKE_MODEL_OPTIONS = ["(Assembled From Parts, Homemade)","(Homemade Motorcycle, Moped.Etc)","(Homemade Trailer)","(Reconstructed Motor Home)",
     "(Reconstructed Trailers)","See Lancia Make)", "(Unlisted Construction Equipment Make)","(Unlisted Farm/Garden Equipment Make)","(Unlisted Make)",
