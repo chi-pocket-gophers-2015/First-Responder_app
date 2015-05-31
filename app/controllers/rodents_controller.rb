6fe4245de64720c50a102645c07e3b7a978ce424
Peter Manhart <petermanhart@gmail.com>
2015-05-31 16:23:05 -0500

    add rodent params to controller

--- app/controllers/rodents_controller.rb
+++ app/controllers/rodents_controller.rb
@@ -1,16 +1,39 @@
 class RodentsController < ApplicationController
   def new
+    session.clear
+    @category = 'rodents'
   end
 
   def create
-
+    session[:lat] = params['lat']
+    session[:lng] = params['lng']
+    session[:street_address] = params['address']
+    session[:zip] = params['zip']
+    @request = Request.new
+    render '/rodents/form'
   end
+
+  def form
+  end
+
 
   def update
-
-  end
-
-  def success
-
+    rodent_params = {
+      "service_code"=> "4fd3b9bce750846c5300004a",
+      "service_name"=>"Rodent Baiting / Rat Complaint",
+      'description'=> params[:description],
+      'address'=> session[:street_address] +
+        ", Chicago, IL, " + session[:zip],
+      'lat'=> session[:lat],
+      'long'=> session[:lng],
+      'attribute[DOYOUWAN]'=> params[:backyard_inspected],
+      'attribute[DOYOUWA1]'=> params[:backyard_baited]
+    }
+    request = Request.new.party_time(rodent_params)
+    binding.pry
+    # params = params.merge{'lat': session['lat'], blah blah}
+    # HTTParty(pothole_params)
+    # Request.new(params)
+    # render '/requests/success'
   end
 end
--- app/models/rodent.rb
+++ app/models/rodent.rb
@@ -1,2 +1,4 @@
 class Rodent < ActiveRecord::Base
+  RODENT_INSPECTION_OPTIONS = ["Inspect Back Yard", "No"]
+  RODENT_BAIT_OPTIONS = ["Bait Back Yard", "No"]
 end
