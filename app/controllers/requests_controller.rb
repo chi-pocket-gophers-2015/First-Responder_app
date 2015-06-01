bbb9228e2af1aa530c433001be8f0c5c5d59a473
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-31 19:13:04 -0500

    removed pry

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -8,7 +8,6 @@ class RequestsController < ApplicationController
     token = params[:token]
     city_params = Request.official_city_data(token)
     @request = Request.new(Request.filter_params(city_params))
-    binding.pry
     if @request.save
       redirect_to request_path(@request)
     end
