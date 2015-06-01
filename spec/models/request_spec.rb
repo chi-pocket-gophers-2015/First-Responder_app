0c895d6b0332d00dcb11732717ad6e5755732358
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-31 19:28:15 -0500

    added 2 tests to request model

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Request, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '#official_city_data should return a hash' do
+    token = '556ba55dff03c04034a04e80'
+    request = Request.official_city_data(token)
+    expect(request).to be_a Hash
+  end
+
+  it '#official_city_data should have a status' do
+    token = '556ba55dff03c04034a04e80'
+    request = Request.official_city_data(token)
+    expect(request['status']).to eq('open')
+  end
 end
