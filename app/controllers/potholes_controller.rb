20801b83f825c032889fb8ecb88711b3c41f8604
huyhuynh408 <huyhuynh85@gmail.com>
2015-06-01 15:50:01 -0500

    updated potholes controller

--- app/controllers/potholes_controller.rb
+++ app/controllers/potholes_controller.rb
@@ -19,22 +19,22 @@ class PotholesController < ApplicationController
 
   def update
     if logged_in?
-      record = RequestRecord.new(image: params['image'], user_id: current_user.id)
+      record = RequestRecord.create(image: params['image'], user_id: current_user.id)
     else
-      record = RequestRecord.new(image: params['image'])
+      record = RequestRecord.create(image: params['image'])
     end
     #need to find out what the route is for pic on heroku
     #Sample url below
     #View image <%= image_tag(@record.image.url(:thumb)) %>
     #/system/request_records/images/000/000/003/original/Laina.jpeg?1433185787
     pothole_params = {
-      "service_code"=> "4fd3b656e750846c53000004",
-      "service_name"=> "Pothole in Street",
-      'description'=> params[:description],
-      'address'=> session[:street_address] +
-        ", Chicago, IL, " + session[:zip],
-      'lat'=> session[:lat],
-      'long'=> session[:lng],
+      'service_code' => '4fd3b656e750846c53000004',
+      'service_name' => 'Pothole in Street',
+      'description' => params[:description],
+      'address' => session[:street_address] +
+        ', Chicago, IL, '' + session[:zip],
+      'lat' => session[:lat],
+      'long' => session[:lng],
       'attribute[WHEREIST]'=> params[:where_located],
       'first_name' => params[:first_name],
       'last_name' => params[:last_name],
@@ -44,6 +44,7 @@ class PotholesController < ApplicationController
     request = Request.new.party_time(pothole_params.merge({street_address: session[:street_address],
       zip_code: session[:zip]}))
     token = request[0]['token']
+    record.update(token: token)
     redirect_to controller: 'requests', action: 'create', token: token
   end
 
