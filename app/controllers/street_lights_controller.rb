adfbe2dc4fcb65ef5b7109c6c851727c029a023e
George Lee <ghrlee@gmail.com>
2015-06-02 11:13:00 -0500

    nothing

--- app/controllers/street_lights_controller.rb
+++ app/controllers/street_lights_controller.rb
@@ -21,10 +21,6 @@ class StreetLightsController < RequestsController
     else
       record = RequestRecord.create(image: params['image'])
     end
-    #need to find out what the route is for pic on heroku
-    #Sample url below
-    #View image <%= image_tag(@record.image.url(:thumb)) %>
-    #/system/request_records/images/000/000/003/original/Laina.jpeg?1433185787
     street_light_params = {
       "service_code"=> "4ffa9f2d6018277d400000c8",
       "service_name"=> "Street Light Out",
@@ -37,7 +33,8 @@ class StreetLightsController < RequestsController
       'first_name' => params[:first_name],
       'last_name' => params[:last_name],
       'email' => params[:email],
-      'phone' => params[:phone]
+      'phone' => params[:phone],
+      'media_url' => record.image.url
     }
     @errors = StreetLight.city_params_missing(street_light_params)
     request = Request.new.party_time(street_light_params.merge({street_address: get_address, zip_code: get_zip}))
