115af8b32b1cdbdea2f2e5ee06e64044b6d9559b
huyhuynh408 <huyhuynh85@gmail.com>
2015-06-03 18:31:20 -0500

    added pagination

--- app/controllers/potholes_controller.rb
+++ app/controllers/potholes_controller.rb
@@ -1,7 +1,7 @@
 class PotholesController < ApplicationController
 
   def show
-    @potholes = Request.where(type_of_service_request: "Pothole in Street").paginate(page: params[:page], per_page: 5)
+    @potholes = Request.where(type_of_service_request: "Pothole in Street").paginate(page: params[:page], per_page: 7)
   end
 
   def new
