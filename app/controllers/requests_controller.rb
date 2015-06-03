5c9854dc68fb6a3db035f945dfbb097d59f708b0
Peter Manhart <petermanhart@gmail.com>
2015-06-03 08:33:03 -0500

    nothing consequential

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -11,12 +11,12 @@ class RequestsController < ApplicationController
 
   def map
     @requests = Request.all
-    @vehicles = Request.where(type_of_service_request: "Abandoned Vehicle Complaint").map{|p| p.to_json}
-    @graffitis = Request.where(type_of_service_request: "Graffiti Removal").map{|p| p.to_json}
-    @potholes = Request.where(type_of_service_request: "Pothole in Street").map{|p| p.to_json}
-    @rodents = Request.where(type_of_service_request: "Rodent Baiting/Rat Complaint").map{|p| p.to_json}
-    @lights = Request.where(type_of_service_request: "Street Light Out").map{|p| p.to_json}
-    @trees = Request.where(type_of_service_request: "Tree Debris").map{|p| p.to_json}
+    @vehicles = Request.where(type_of_service_request: "Abandoned Vehicle Complaint")
+    @graffitis = Request.where(type_of_service_request: "Graffiti Removal")
+    @potholes = Request.where(type_of_service_request: "Pothole in Street")
+    @rodents = Request.where(type_of_service_request: "Rodent Baiting/Rat Complaint")
+    @lights = Request.where(type_of_service_request: "Street Light Out")
+    @trees = Request.where(type_of_service_request: "Tree Debris")
   end
 
   def create
